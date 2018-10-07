6ef491160b4a1132a0f447488e0d6d531ee2e66f
Admin <zadow.benjami@uwlax.edu>
2018-10-07 18:58:22 -0500

    -Ben Pushing newest version of Sqlite class as requested by  Nick W.

--- Shopping Cart/src/JdbcSQLiteConnection.java
+++ Shopping Cart/src/JdbcSQLiteConnection.java
@@ -1,22 +1,22 @@
 
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.*;
- 
+
 /**
  * This program demonstrates making JDBC connection to a SQLite database.
  * @author www.codejava.net
  *
  */
 public class JdbcSQLiteConnection {
- 
+	private static String dbURL = "jdbc:sqlite:UsersDb.db";
+  private static Connection conn;
+  private String insert = "INSERT INTO UsersDb(Username, Password, Status) VALUES(?,?,?)";
+  
     public static void main(String[] args) {
         try {
             Class.forName("org.sqlite.JDBC");
             String dbURL = "jdbc:sqlite:UsersDb.db";
-            Connection conn = DriverManager.getConnection(dbURL);
+            conn = DriverManager.getConnection(dbURL);
             if (conn != null) {
                 System.out.println("Connected to the database");
                 DatabaseMetaData dm = (DatabaseMetaData) conn.getMetaData();
@@ -33,6 +33,26 @@ public class JdbcSQLiteConnection {
         }
     }
     void addUserToDatabase(User newUser) {
-    	//MUST HASH PASSWORD 
+    	//MUST HASH PASSWORD
+    	
+   
+      String userName = newUser.userName;
+      String userPass = newUser.password;
+      String city = newUser.city;
+      String state = newUser.state;
+      String zip = newUser.zipCode;
+      String credit = newUser.creditCard;
+      int userType = newUser.userType;
+      try {
+    	conn = DriverManager.getConnection(dbURL);
+    	Statement statement = conn.createStatement();
+      	statement.executeUpdate(insert);
+        statement.executeUpdate("INSERT INTO UsersDb " + "VALUES ("+ newUser.userName + ", " + newUser.password + ", " + newUser.userType + ")");
+        conn.close();
+      } catch (SQLException e) {
+        e.printStackTrace();
+      }
+     
     }
+    
 }
